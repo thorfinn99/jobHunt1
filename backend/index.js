ed6451c0fe15bf222765806de98595cd26bb5f08
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2024-11-18 18:32:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin: 'http://localhost:5173',
+    'https://jobhuntfrontend.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions))
