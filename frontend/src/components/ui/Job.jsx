e9ddebcc8d340a43707d2531c066b200ee183d36
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2025-03-14 00:56:07 +0530

    Update Job.jsx

--- frontend/src/components/ui/Job.jsx
+++ frontend/src/components/ui/Job.jsx
@@ -14,7 +14,7 @@ function Job({job}) {
  
   const detailsShow = async () => { 
     try { 
-      const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {withCredentials:true} ) 
+      const res = await axios.get(`${import.meta.env.VITE_JOB_API_END_POINT}/get/${jobId}`, {withCredentials:true} ) 
       if(res.data.success){ 
         dispatch(setSingleJob(res.data.job))  
       } 
