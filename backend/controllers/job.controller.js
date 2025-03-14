2f2222aafb01693d709a3188bff9b1f676c98f08
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2025-03-14 20:59:25 +0530

    Update job.controller.js

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -35,7 +35,7 @@ export const postJob = async (req,res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            message: "Server error",
+            message: `${error.message}`,
             success: false
         });
     }
