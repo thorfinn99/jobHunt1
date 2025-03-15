57b28a775beaff91c0df0904734a7b53709320f1
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2025-03-15 13:42:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/job.route.js"
-import ApplicationRoute from "./routes/application.route.js"
+import applicationRoute from "./routes/application.route.js"
 
 
 dotenv.config({});
@@ -52,7 +52,7 @@ const PORT = process.env.PORT || 8000;
 app.use("/api/v1/user", userRoute)
 app.use("/api/v1/company", companyRoute)
 app.use("/api/v1/job", jobRoute)
-app.use("/api/v1/application", ApplicationRoute)
+app.use("/api/v1/application", applicationRoute)
 
 app.listen(PORT, ()=> {
     connectDB();
